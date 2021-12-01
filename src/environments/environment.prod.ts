36d95f09f55be2d803c4bddd547a18edf1b40247
Dang Khoi Nguyen <knguy3nd@gmail.com>
2021-12-01 10:35:26 -0800

    Adapted to AWS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'https://dw0712vu66.execute-api.us-east-1.amazonaws.com'
 };
